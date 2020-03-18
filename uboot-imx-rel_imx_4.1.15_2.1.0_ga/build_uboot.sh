ced53ef4a2c85c41677f458b56e58d2341a795b3
topeet <topeet@topeet.com>
2020-03-17 18:18:52 -0700

    init uboot code

---
+++ uboot-imx-rel_imx_4.1.15_2.1.0_ga/build_uboot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export ARCH=arm
+
+export CROSS_COMPILE=/usr/local/arm/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi-
+
+make distclean
+
+#make mx6ul_topeet_defconfig
+if   [ "$1" = "nand" ]
+then
+        make mx6ul_topeet_nand_defconfig
+else
+        make mx6ul_topeet_emmc_defconfig
+fi
+
+make 2>&1
